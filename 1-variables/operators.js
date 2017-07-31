bba176701a45a57a918b926c55891272c164e18a
Luke Schlangen <lukeschlangen@gmail.com>
2017-07-31 10:36:24 -0500

    variable rename

--- 1-variables/operators.js
+++ 1-variables/operators.js
@@ -1,7 +1,7 @@
-// 1 - Assign the value 17 to a variable called 'pi'
+// 1 - Assign the value 17 to a variable called 'someNumber'
 
-// 2 - Create a new variable called peeps and assign it the value of pi
+// 2 - Create a new variable called peeps and assign it the value of someNumber
 
 // 3 - Decrease the value of peeps by 1
 
-// 4 - Write a console log that checks if pi is equal to peeps
+// 4 - Write a console log that checks if someNumber is equal to peeps
